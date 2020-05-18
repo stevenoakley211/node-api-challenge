496b4943dda6e0094d7f7201e3ee4a150c09d97c
stevenoakley211 <stevenoakley211@live.com>
2020-05-17 23:35:16 -0500

    mvp

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -40,15 +40,64 @@ router.get('/:id',(req, res)=>{
 })
 
 router.post('/', (req, res)=>{
+    if(req.body.name & req.body.description){
+        
+        db.insert(req.body)
+            .then(project=>{
+              res.status(200).json({message:"successfully added project"})  
+            })
+            .catch(err=>{
+                res.status(500).json(
+                        { error:"server error" }
+                    )
+            })
+        
+    }
+    else{res.status(404).json(
+                { error:"please supply name and description in the request" }
+            )}
 
 })
 
 router.put('/',(req, res)=>{
-
+    const id = req.params.id
+    const body = req.body
+    if(req.body.name & req.body.description){
+        db.update(id, body)
+            .then(project=>{
+                res.status(200).json(project)
+            })
+            .catch(err=>{
+                res.status(500).json(
+                    { error:"server error" }
+                )
+            })
+        }
+        else{
+            res.status(404).json(
+                { error: "please supply name and description in the request" }
+            )
+        }
+        
 })
 
 router.delete('/',(req, res)=>{
-
+    const id = req.params.id;
+    db.delete(id)
+    .then(project=>{
+        if (project){
+            res.status(200).json({message:"project deleted "})
+        }
+        else{
+            res.status(200).json({message:"project deleted "})
+        }
+        
+    })
+    .catch(err=>{
+        res.status(500).json(
+                    { error:"server error" }
+                )
+    })
 })
 
 module.exports = router
